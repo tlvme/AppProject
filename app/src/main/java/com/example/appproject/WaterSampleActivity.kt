82c90464cdb3926d66d45e192566eae238593b3e
tlvme <sbadoni19@gmail.com>
2020-02-26 12:47:11 +0530

    updated structure of app

---
+++ app/src/main/java/com/example/appproject/WaterSampleActivity.kt
@@ -0,0 +1,12 @@
+package com.example.appproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WaterSampleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_water_sample)
+    }
+}
