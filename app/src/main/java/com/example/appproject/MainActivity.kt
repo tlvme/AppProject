efe5626750821bacc814d5c21508af08644dd572
tlvme <sbadoni19@gmail.com>
2020-02-26 13:31:58 +0530

    changed water sample activity

--- app/src/main/java/com/example/appproject/MainActivity.kt
+++ app/src/main/java/com/example/appproject/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.appproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    fun ButtonClick(view: View) {
+        val myIntent = Intent(this, WaterSampleActivity::class.java)
+        startActivity(myIntent)
     }
 }
