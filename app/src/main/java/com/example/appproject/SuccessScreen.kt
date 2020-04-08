7e4ea7abf304b05cbeb23c173cead5c81f2d2b6e
tlvme <sbadoni19@gmail.com>
2020-04-08 11:42:38 +0530

    added navigation

--- app/src/main/java/com/example/appproject/SuccessScreen.kt
+++ app/src/main/java/com/example/appproject/SuccessScreen.kt
@@ -1,12 +1,21 @@
 package com.example.appproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class SuccessScreen : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_success_screen)
+    }
+
+    fun ButtonClickSuccess(view: View) {
+
+        val myIntent = Intent(this, MainActivity::class.java)
+        myIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+        startActivity(myIntent)
     }
 }
